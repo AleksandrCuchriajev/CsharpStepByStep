90aa484cdbce4204c042f4156866972eb136ca30
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-10 06:50:31 +0200

    Initial commit

---
+++ Course-materials/Step-1-First-program-Comments/1-1-Hello-world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_1_Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello,Alex!");
+            Console.Read();
+        }
+    }
+}
