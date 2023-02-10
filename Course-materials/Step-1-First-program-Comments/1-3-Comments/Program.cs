ff84917612a30e8746f89b39941b32459fe5132d
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-10 07:09:38 +0200

    Initial commit

---
+++ Course-materials/Step-1-First-program-Comments/1-3-Comments/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1_3_Comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Charp comments
+
+            // This is a comment
+            /* 
+             The code below will print the words Hello World
+             to the screen, and it is amazing 
+            */
+            Console.WriteLine("Hello World!"); // This is a comment
+
+            // Use Ctrl+/ to comment and uncomment the line/lines
+            // Console.WriteLine("Hello,Alex"); 
+
+            /// <summary>
+            /// This is an XML comment
+            /// </summary>
+            Console.Read();
+        }
+    }
+}
