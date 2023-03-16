b1d072d92424d207e0bf7397635617f1c3da3f37
Acoptex <acoptex@gmail.com>
2023-03-16 09:29:22 +0200

    new folder added to exercises

---
+++ Exercises/Exercises-Step-3/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Exercises_Step_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1. 
+
+
+
+
+
+
+        }
+    }
+}
